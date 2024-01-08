2039e67c0858b37b0bda1539ebbf570f4772b495
lee_c <gitleelee@gmail.com>
2024-01-07 19:28:20 -0800

    Fixed data, now easier to digest and summarize.

--- data-probability/main.py
+++ data-probability/main.py
@@ -23,5 +23,10 @@ for index in range(0, len(fare)):
     fare_not_survived.append(fare[index])
 
 #ADD CODE: print lists
-print(fare_survived)
-print(fare_not_survived)
+print(f"The average fare of those who survived was: ${round(np.mean(fare_survived), 2)}")
+
+print(f"The average fare of those who did not survive was: ${round(np.mean(fare_not_survived), 2)}")
+
+print(f"The median fare of those who survived was: ${round(np.median(fare_survived), 2)}")
+
+print(f"The median fare of those who did not survive was: ${round(np.median(fare_not_survived), 2)}")
