f092a7611265c3f994f8284019aaf111d0a224f8
lee_c <gitleelee@gmail.com>
2024-01-08 14:56:32 -0800

    Scraped h2, a, and img from wikipedia.

--- web-scraper/main.py
+++ web-scraper/main.py
@@ -6,3 +6,15 @@ honey_badger.raise_for_status()
 honey_badger_html = honey_badger.text.encode("utf-8")
 honey_badger_soup = BeautifulSoup(honey_badger_html, 'html.parser')
 
+h2 = honey_badger_soup.find_all("h2")
+print(len(h2))
+
+for element in h2:
+    print(element)
+    
+# first 5 links
+links = honey_badger_soup.find_all("a")
+print (links[:5])
+# slice last 4 imgs
+images = honey_badger_soup.find_all("img")
+print(images[-4:])
