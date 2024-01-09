3812fc89a5042eb3bf0b44fc9df0b3922291568c
lee_c <gitleelee@gmail.com>
2024-01-09 10:08:21 -0800

    Created a mosaic plot.

--- clean-data/main.py
+++ clean-data/main.py
@@ -1,11 +1,42 @@
-import json
+import pandas as pd
+import matplotlib.pyplot as plt
 import re
+import json
+from statsmodels.graphics.mosaicplot import mosaic
 
 with open("data.json", "r") as text:
     data = json.load(text)
 
-# removes extra data ".mw-parser-output .nobold{font-weight:normal}(EX)\u00a0" from data.json for clarity.
 for item in data:
   item["Category"] = re.compile(" [\.(]").split(item["Category"])[0]
 
-print(data)
+#print(data)
+
+classes = ["Mammalia", "Aves", "Reptilia"]
+statuses = ["Endangered", "Critically endangered", "Vulnerable"]
+
+mosaic_data = []
+for item in data:
+  if item["Animal Class"] in classes and item["Category"] in statuses:
+    mosaic_data.append(item)
+
+properties = {
+  "Endangered": {"color": "#FACDB6"},
+  "Critically endangered": {"color": "#C5CADE"},
+  "Vulnerable": {"color": "#A8DBD2"},
+}
+
+plt.rc("font", size=8)
+
+mosaic_dataframe = pd.DataFrame(mosaic_data)
+
+fig = mosaic(
+    mosaic_dataframe,
+    ["Category","Animal Class"],
+    title="Conservation Status by Animal Class",
+    gap=[0.02, 0.02],
+    axes_label=True,
+    properties=lambda x: properties[x[0]],
+)
+
+plt.savefig("endangered_species.png")
