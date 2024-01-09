7b4fd6dcb9fd93b6f96fabd892651bb850eea63a
lee_c <gitleelee@gmail.com>
2024-01-09 10:55:58 -0800

    Design graph using sns.scatterplot function.

--- goodreads-data/main.py
+++ goodreads-data/main.py
@@ -5,4 +5,4 @@ import matplotlib.pyplot as plt
 with open("books.csv","r") as datafile:
     data = pd.read_csv(datafile,delimiter=",")
 
-print(data["language_code"])
+sns.scatterplot(x="average_rating", y="# num_pages", size="# num_pages", sizes=(20, 180), data=data)
