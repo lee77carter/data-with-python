9c005b2f445674971b862ccc2f9b36719fb93f2f
lee_c <gitleelee@gmail.com>
2024-01-09 09:35:45 -0800

    Added comments explaining data clean results.

--- clean-data/main.py
+++ clean-data/main.py
@@ -4,6 +4,7 @@ import re
 with open("data.json", "r") as text:
     data = json.load(text)
 
+# removes extra data ".mw-parser-output .nobold{font-weight:normal}(EX)\u00a0" from data.json for clarity.
 for item in data:
   item["Category"] = re.compile(" [\.(]").split(item["Category"])[0]
 
